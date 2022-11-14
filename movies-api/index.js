ce87f3fe035bd15fe5cd41d11328bf4b127f2006
chenghao xie <1152759491@qq.com>
2022-11-14 18:50:30 +0000

    Routes for get movie and movie details

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
+import moviesRouter from './api/movies';
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
 
-app.use(express.static('public'));
+app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
