427a42503d4d780ad9ce706eab0e17629d21480b
chenghao xie <1152759491@qq.com>
2022-12-03 15:33:18 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
 import './db';
 import './seedData';
 dotenv.config();
@@ -12,6 +13,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
+app.use('/api/users', usersRouter);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
