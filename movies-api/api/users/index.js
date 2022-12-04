13501324cf36092919dcf7a392857dad1e35c5c4
chenghao xie <1152759491@qq.com>
2022-12-04 14:17:17 +0000

    Mongo lab Error Handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from './userModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -10,23 +11,23 @@ router.get('/', async (req, res) => {
 });
 
 // register(Create)/Authenticate User
-router.post('/', async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
     if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
+        await User(req.body).save()
         res.status(201).json({
             code: 201,
             msg: 'Successful created new user.',
         });
     }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
+    else {  //Must be authenticating then! Query the DB and check if there's a match
         const user = await User.findOne(req.body);
         if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
         }
     }
-});
+}));
 router.put('/:id', async (req, res) => {
     if (req.body._id) delete req.body._id;
     const result = await User.updateOne({
@@ -59,4 +60,5 @@ router.get('/:id/favourites', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
     }
 });
+
 export default router;
--- movies-api/index.js
+++ movies-api/index.js
@@ -6,7 +6,14 @@ import usersRouter from './api/users';
 import './db';
 import './seedData';
 dotenv.config();
-
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
 const app = express();
 
 const port = process.env.PORT;
@@ -14,6 +21,7 @@ app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
+app.use(errHandler);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
