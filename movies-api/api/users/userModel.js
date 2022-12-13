0d56fe3de0fbb801351c4501afc5b7722377f840
chenghao xie <1152759491@qq.com>
2022-12-13 16:22:28 +0000

    Topic 11 - Session based Auth

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -12,5 +12,16 @@ const MovieSchema = new Schema({
     password: {type: String, required: true },
     favourites: [MovieSchema]
   });
+  UserSchema.statics.findByUserName = function (username) {
+    return this.findOne({ username: username });
+  };
+  
+  UserSchema.methods.comparePassword = function (candidatePassword) {
+    const isMatch = this.password === candidatePassword;
+    if (!isMatch) {
+      throw new Error('Password mismatch');
+    }
+    return this;
+  };
 
 export default mongoose.model('User', UserSchema);
--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,9 @@ import genresRouter from './api/genres';
 import usersRouter from './api/users';
 import './db';
 import './seedData';
+import session from 'express-session';
+import authenticate from './authenticate';
+
 dotenv.config();
 const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
@@ -15,12 +18,18 @@ const errHandler = (err, req, res, next) => {
   res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
 };
 const app = express();
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
 
 const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 app.use(errHandler);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
