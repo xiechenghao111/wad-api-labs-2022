fff6cd86d0b49cf683e4034425a9ed6e6cbee0ee
chenghao xie <1152759491@qq.com>
2022-12-13 18:53:23 +0000

    Topic 12 - Finished!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,6 +3,7 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import { getUpcomingMovies } from '../tmdb-api';
 const router = express.Router(); 
 
 router.get('/', asyncHandler(async (req, res) => {
@@ -50,4 +51,8 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 export default router;
